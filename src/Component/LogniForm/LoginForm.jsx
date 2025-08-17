8b4a431a149e808996737b2f0e6d80b602c817f1
Gargi Richhariya <gargirichhariya112>
2025-08-17 16:30:38 +0530

    update

--- src/Component/LogniForm/LoginForm.jsx
+++ src/Component/LogniForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState }from 'react';
+import { useState } from 'react';
 import './LoginForm.css';
 
 
@@ -6,21 +6,21 @@ function LoginForm() {
   const [isSignup, setIssSignup] = useState(true);
   return (
     <div className="container">
-
-      <div className="form-box box">
+      <div className="box">
+        <div className="form-box">
         <h1 id="title">{isSignup ? "SignUp" :" Log In"} </h1>
         <form className="form">
           <div className="input-group ">
             {isSignup && (
-              <div className="input-field box">
+              <div className="input-field ">
               <input type="text" placeholder="Name" />
             </div>
             )}
             
-            <div className="input-field box">
+            <div className="input-field ">
               <input type="email" placeholder="Email" />
             </div>
-            <div className="input-field box">
+            <div className="input-field ">
               <input type="password" placeholder="Password" />
             </div>
           </div>
@@ -37,6 +37,8 @@ function LoginForm() {
           
         </form>
       </div>
+      </div>
+      
     </div>
   );
 }
